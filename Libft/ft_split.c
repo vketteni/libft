cd99ee58e070206993aa9c4b4ffb7c092122dc41
Vincent Ketteniss <vketteni@c4b12c1.42berlin.de>
2023-11-28 17:59:46 +0100

    adding tests

--- Libft/ft_split.c
+++ Libft/ft_split.c
@@ -14,8 +14,36 @@
 #include "libft.h"
 #include <stddef.h>
 
-char **ft_get_result_array(const char *s, char c);
-void	ft_allocation_error(char **array);
+void	ft_allocation_error(char **array)
+{
+	int	i;
+
+	i = 0;
+	while (array[i] != 0)
+		free(array[i++]);
+	free(array);
+}
+
+char	**ft_get_result_array(char const *s, char c)
+{
+	char	**result;
+	char const	*ptr;
+	int	delimiter_count;
+
+	delimiter_count = 0;
+	ptr = s;
+	while (*ptr++)
+	{
+		if (*ptr == c)
+			delimiter_count++;
+	}
+
+	result = (char **)malloc(sizeof(char *) * (delimiter_count + 1) + 1);
+	if (result == 0)
+		return (0);
+	result[delimiter_count + 1] = 0;
+	return (result);
+}
 
 char	**ft_split(char const *s, char c)
 {
@@ -54,50 +82,73 @@ char	**ft_split(char const *s, char c)
 	return (result);
 }
 
-void	ft_allocation_error(char **array)
+
+
+#include <stdio.h>
+#include <string.h>
+#include <stdlib.h>
+
+// Function to free the memory allocated for the result of ft_split
+void	free_split_result(char **result)
 {
-	int	i;
-
-	i = 0;
-	while (array[i] != 0)
-		free(array[i++]);
-	free(array);
-}
-
-char	**ft_get_result_array(char const *s, char c)
-{
-	char	**result;
-	char const	*ptr;
-	int	delimiter_count;
-
-    delimiter_count = 0;
-	ptr = s;
-	while (*ptr++)
-	{
-		if (*ptr == c)
-			delimiter_count++;
-	}
-
-	result = (char **)malloc(sizeof(char *) * (delimiter_count + 1) + 1);
-	if (result == 0)
-		return (0);
-	result[delimiter_count + 1] = 0;
-	return (result);
-}
-
-/*
-int	main()
-{
-	char	**result;
-	int i;
-	char string[] = "-he-l--lo-";
-	result = ft_split(string, '-');
-	i = 0;
+	int i = 0;
 	while (result[i])
 	{
-		printf("result[%d] = '%s'", i, result[i]);
+		free(result[i]);
 		i++;
 	}
-	return (0);
+	free(result);
 }
-*/
+
+int main(void)
+{
+	// Test Case 1: Testing split on an empty string
+	printf("Test Case 1: Testing split on an empty string.\n");
+	char *test_1 = "";
+	char **result_1 = ft_split(test_1, ' ');
+	char *expected_1[] = {NULL};
+	printf("String:\t\t'%s'\nDelimiter:\t'%s'\nResult:\t\t'%p'\nExpected:\t'%p'\nPass:\t\t%s\n\n", test_1, " ", (void*)result_1, (void*)expected_1, result_1[0] == expected_1[0] ? "Yes" : "No");
+	free_split_result(result_1);
+
+	// Test Case 2: Testing split with a char that isn't in string
+	printf("Test Case 2: Testing split with a char that isn't in string.\n");
+	char *test_2 = "ThisIsAString";
+	char **result_2 = ft_split(test_2, ' ');
+	char *expected_2[] = {"ThisIsAString", NULL};
+	printf("String:\t\t'%s'\nDelimiter:\t'%s'\nResult:\t\t'%s'\nExpected:\t'%s'\nPass:\t\t%s\n\n", test_2, " ",result_2[0], expected_2[0], strcmp(result_2[0], expected_2[0]) == 0 && result_2[1] == expected_2[1] ? "Yes" : "No");
+	free_split_result(result_2);
+
+	// Test Case 3: Testing split with a char that is in string
+	printf("Test Case 3: Testing split with a char that is in string.\n");
+	char *test_3 = "Split,This,String";
+	char **result_3 = ft_split(test_3, ',');
+	char *expected_3[] = {"Split", "This", "String", NULL};
+	printf("String:\t\t'%s'\nDelimiter:\t'%s'\nResult:\t\t'%s' '%s' '%s'\nExpected:\t'%s' '%s' '%s'\nPass:\t\t%s\n\n", test_3, ",",result_3[0], result_3[1], result_3[2], expected_3[0], expected_3[1], expected_3[2], strcmp(result_3[0], expected_3[0]) == 0 && strcmp(result_3[1], expected_3[1]) == 0 && strcmp(result_3[2], expected_3[2]) == 0 && result_3[3] == expected_3[3] ? "Yes" : "No");
+	free_split_result(result_3);
+
+	// Test Case 4: Testing split with a match on the first char in string
+	printf("Test Case 4: Testing split with a match on the first char in string.\n");
+	char *test_4 = "FirstMatchInTheString";
+	char **result_4 = ft_split(test_4, 'F');
+	char *expected_4[] = {"", "irstMatchInTheString", NULL};
+	printf("String:\t\t'%s'\nDelimiter:\t'%s'\nResult:\t\t'%s' '%s'\nExpected:\t'%s' '%s'\nPass:\t\t%s\n\n", test_4, "F",result_4[0], result_4[1], expected_4[0], expected_4[1], strcmp(result_4[0], expected_4[0]) == 0 && strcmp(result_4[1], expected_4[1]) == 0 && result_4[2] == expected_4[2] ? "Yes" : "No");
+	free_split_result(result_4);
+
+	// Test Case 5: Testing split with a match on the final char in string
+	printf("Test Case 5: Testing split with a match on the final char in string.\n");
+	char *test_5 = "StringWithMatchZ";
+	char **result_5 = ft_split(test_5, 'Z');
+	char *expected_5[] = {"StringWithMatch", "", NULL};
+	printf("String:\t\t'%s'\nDelimiter:\t'%s'\nResult:\t\t'%s' '%s'\nExpected:\t'%s' '%s'\nPass:\t\t%s\n\n", test_5, "Z",result_5[0], result_5[1], expected_5[0], expected_5[1], strcmp(result_5[0], expected_5[0]) == 0 && strcmp(result_5[1], expected_5[1]) == 0 && result_5[2] == expected_5[2] ? "Yes" : "No");
+	free_split_result(result_5);
+
+	// Test Case 6: Testing split with a match on two successive chars in string
+	printf("Test Case 6: Testing split with a match on two successive chars in string.\n");
+	char *test_6 = "SuccessiveMatch";
+	char **result_6 = ft_split(test_6, 's');
+	char *expected_6[] = {"Succe","","iveMatch", NULL};
+	printf("String:\t\t'%s'\nDelimiter:\t'%s'\nResult:\t\t'%s' '%s' '%s'\nExpected:\t'%s' '%s' '%s'\nPass:\t\t%s\n\n", test_6, "s",result_6[0], result_6[1], result_6[2], expected_6[0], expected_6[1], expected_6[2], strcmp(result_6[0], expected_6[0]) == 0 && strcmp(result_6[1], expected_6[1]) == 0 && strcmp(result_6[2], expected_6[2]) == 0 && result_6[3] == expected_6[3] ? "Yes" : "No");
+	free_split_result(result_6);
+
+	return 0;
+}
